4a536535902940335052035fd852ce6d9cb9d750
Patryk Kurek <patrykkurek@student.agh.edu.pl>
2025-10-05 13:47:57 +0200

    feat: storage for photos in supabase

--- harwood-cms/config/middlewares.ts
+++ harwood-cms/config/middlewares.ts
@@ -1,9 +1,32 @@
-export default [
-  'strapi::logger',
+export default ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // cloudinary images
+            'lh3.googleusercontent.com', // google avatars
+            'platform-lookaside.fbsbx.com', // facebook avatars
+            'dl.airtable.com', // strapi marketplace,
+            "market-assets.strapi.io",
+            env('SUPABASE_API_URL'),
+          ],
+          'media-src': ["'self'", 'data:', 'blob:', env('SUPABASE_API_URL')],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
